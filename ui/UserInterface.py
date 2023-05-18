5c79a4981d6b6ef8fc11beac3335545f8d846c17
Gabriele <g.pandini@campus.unimib.it>
2023-05-18 13:09:47 +0200

    Changed UI and added tryExcept

--- ui/UserInterface.py
+++ ui/UserInterface.py
@@ -1,11 +1,12 @@
 import os
 import tkinter
-from tkinter import StringVar, OptionMenu, W, TRUE, FALSE
+from tkinter import W, TRUE, FALSE
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 
 import customtkinter
 import numpy as np
+from customtkinter import StringVar
 
 from methods.IterativeMethod import IterativeMethod
 from methods.validation.Validate import Validate
@@ -28,7 +29,7 @@ class UserInterface:
             1e-10,
         ]
         self.root = customtkinter.CTk()
-        self.height = 450
+        self.height = 480
         self.width = 500
         posx = (self.root.winfo_screenwidth() // 2) - (self.width // 2)
         posy = (self.root.winfo_screenheight() // 2) - (self.height // 2)
@@ -58,17 +59,17 @@ class UserInterface:
         frame2.pack(fill="both", expand=TRUE, pady=5, padx=100)
 
         customtkinter.CTkLabel(master=frame2, text="Method:").grid(row=1, column=1, sticky=W)
-        customtkinter.CTkOptionMenu(frame2, values = [
+        customtkinter.CTkOptionMenu(frame2, values=[
             "Jacobi",
             "GaussSeidel",
             "Gradient",
             "Conjugate"
-            ],
-            hover = False,
-            width = 85,
-            height = 22,
-            text_color = 'white',
-            fg_color = '#555555', variable=self.clicked).grid(row=1, column=2)
+        ],
+        hover=False,
+        width=85,
+        height=22,
+        text_color='white',
+        fg_color='#555555', variable=self.clicked).grid(row=1, column=2)
 
         customtkinter.CTkLabel(master=frame2, text="Matrix:").grid(row=2, column=1, sticky=W)
         self.open_button = customtkinter.CTkButton(
@@ -85,18 +86,13 @@ class UserInterface:
 
         self.open_button.grid(row=2, column=2)
         customtkinter.CTkLabel(master=frame2, text="Tolerance:").grid(row=3, column=1, sticky=W)
-        customtkinter.CTkOptionMenu(frame2, values = [
-            "1e-4",
-            "1e-6",
-            "1e-8",
-            "1e-10",
-            ],
-            hover = False,
-            width=85,
-            height=22,
-            text_color='white',
-            fg_color='#555555', variable=self.tolerance).grid(row=3, column=2)
-
+        self.entry = customtkinter.CTkEntry(master=frame2,
+                                            placeholder_text="Entry tol...",
+                                            width=85,
+                                            height=22,
+                                            border_width=2,
+                                            corner_radius=5)
+        self.entry.grid(row=3, column=2, sticky=W)
 
         frame3 = customtkinter.CTkFrame(master=frame)
         frame3.pack(pady=20, padx=50, fill="both", expand=TRUE)
@@ -130,19 +126,36 @@ class UserInterface:
 
     def calculate(self):
         filename = self.matrix.get()
-        tol = float(self.tolerance.get())
+        try:
+            tol = float(self.entry.get())
+        except:
+            tkinter.messagebox.showerror(title=None, message='Tolerance not valid!')
+            return
+
         if filename == '':
             tkinter.messagebox.showerror(title=None, message='File not selected!')
+            return
+        if tol == 0:
+            tkinter.messagebox.showerror(title=None, message='Tolerance not selected!')
+            return
+
+        try:
+            a, n, _ = IterativeMethod.read_matrix(filename)
+        except:
+            tkinter.messagebox.showerror(title=None, message='Runtime error')
             return
         self.error.set('Calculating...')
         self.times.set('Calculating...')
         self.iter.set('Calculating...')
         self.root.update_idletasks()
-        a, n, _ = IterativeMethod.read_matrix(filename)
         x = np.ones(n)
         b = a @ x
         tmp = str(self.clicked.get())
-        err, it, tf = Validate.validate_method(tmp, a, b, tol, x)
+        try:
+            err, it, tf = Validate.validate_method(tmp, a, b, x, tol)
+        except:
+            tkinter.messagebox.showerror(title=None, message='Runtime error')
+            return
         self.error.set(str(err))
         self.times.set(str("%.5f" % tf))
         self.iter.set(str(it))
